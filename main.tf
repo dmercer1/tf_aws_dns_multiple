6895e68f94af0cc4eacd257f014853859a943f4f
drew mercer <dmercer@crowdcompass.com>
2016-02-19 13:41:23 -0800

    Var names updated

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "dns_entry" {
   source = "github.com/dmercer1/tf_aws_dns"
   zone_id = "${var.zone_id}"
-  names = "${element(split(",", var.names), count.index)}"
-  records = "${var.records}"
+  name = "${element(split(",", var.names), count.index)}"
+  record = "${var.records}"
   ttl = "300"
   type = "${var.type}"
   count = "${length(compact(split(",", var.names)))}"
