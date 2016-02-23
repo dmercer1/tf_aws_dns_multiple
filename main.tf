ad2442e67d1ae0bbd484594e30bfaf24371f0d57
drew mercer <dmercer@crowdcompass.com>
2016-02-23 10:11:44 -0800

    Fixes.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "entry" {
   zone_id = "${var.zone_id}"
   name = "${element(split(",", var.names), count.index)}"
-  record = "${var.records}"
+  record = ["${var.records}"]
   ttl = "300"
-  type = ["${var.type}"]
+  type = "${var.type}"
   count = "${length(compact(split(",", var.names)))}"
 }
